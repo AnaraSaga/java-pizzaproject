6844eb61d73d87abb59805c8ed917d07033dc78a
Anara <anarasaga@gmail.com>
2023-02-22 08:34:42 +0100

    admin for cafes done, admin for pizzas started.

--- src/main/java/de/telran/pizzaProject/controller/MainController.java
+++ src/main/java/de/telran/pizzaProject/controller/MainController.java
@@ -54,24 +54,66 @@ public class MainController {
 
     }
 
-    @GetMapping("/admin")
+    @GetMapping("/cafes")
     public String admin(Model model) {
         model.addAttribute("cafes", cafeRepository.findAll());
-        return "admin";
+        return "cafes";
     }
 
 
     @PostMapping("/addCafe")
     public String addCafe(Cafe cafe, Model model) {
         cafeRepository.save(cafe);
-        return "redirect:/admin";
+        return "redirect:/cafes";
     }
 
-    @GetMapping("/addCafe")  //for btn add
+    @GetMapping("/addCafe")  //for btn submit
     public String addCafe (Model model){
         Cafe cafe = new Cafe();
         model.addAttribute("cafe", cafe);
         return "add_cafe";
+    }
+
+    @GetMapping("/editCafe/{id}") // for btn edit -edit cafe by id
+    public String editCafe(@PathVariable String id, Model model){  // find id in path
+        Cafe cafe = cafeRepository.findById(id).get();
+        model.addAttribute("cafe", cafe);
+        return "add_cafe";
+    }
+
+    @GetMapping("/deleteCafe/{id}") // for btn delete -delete cafe by id
+    public String deleteCafe(@PathVariable String id, Model model){  // find id in path
+        cafeRepository.deleteById(id);
+        model.addAttribute("cafe", cafeRepository.findAll());
+        return "redirect:/cafes";
+    }
+
+    // to add new pizza
+    @PostMapping("/addPizza")
+    public String addPizza (Pizza pizza, Model model) {
+        pizzaRepository.save(pizza);
+        return "redirect:/pizzas";
+    }
+
+    @GetMapping("/addPizza")  //for btn submit
+    public String addPizza (Model model){
+        Pizza pizza = new Pizza();
+        model.addAttribute("pizza", pizza);
+        return "add_pizza";
+    }
+
+    @GetMapping("/editPizza/{id}") // for btn edit -edit cafe by id
+    public String editPizza(@PathVariable String id, Model model){  // find id in path
+        Pizza pizza = pizzaRepository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "add_pizza";
+    }
+
+    @GetMapping("/deletePizza/{id}") // for btn delete -delete cafe by id
+    public String deletePizza(@PathVariable String id, Model model){  // find id in path
+        pizzaRepository.deleteById(id);
+        model.addAttribute("pizza", pizzaRepository.findAll());
+        return "redirect:/pizzas";
     }
 
 }
